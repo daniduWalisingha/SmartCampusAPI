56f8287795d48cf91cb0a733131c782a91fadafc
daniduWalisingha <dwalisinghe@gmail.com>
2026-04-21 15:58:40 +0530

    Add global exception mapper for unhandled API errors

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,29 @@
+package com.smartcampus.filter;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info("Incoming Request: "
+                + requestContext.getMethod()
+                + " "
+                + requestContext.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info("Outgoing Response Status: " + responseContext.getStatus());
+    }
+}
